5d9e666b902b9970600610102c6d07ce7f224ab9
matheusoliandrade <matheus.andradejund@gmail.com>
2024-09-26 09:04:13 -0300

    Cadastro de Usuários e Autenticação com JWT e Bcrypt

--- db/database.js
+++ db/database.js
@@ -4,6 +4,14 @@ const db = new sqlite3.Database('lista-tarefas.db');
 
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)");
+    db.run(`
+  CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    role TEXT
+  )
+`);
 });
 
 module.exports = db;
