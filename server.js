65d1c6d358d64997c871b0ca20abebef5f920759
matheusoliandrade <matheus.andradejund@gmail.com>
2024-09-26 08:50:07 -0300

    Refatorando e Organizando Backend com Boas Práticas de Desenvolvimento

--- server.js
+++ server.js
@@ -1,82 +1,8 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 const PORT = 3000;
-
-app.use(express.json());
-
-// Cria um banco de dados SQLite em memória
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('lista-tarefas.db');
-
-// Cria a tabela 'tarefas' no banco de dados
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, tarefa TEXT)");
-});
-
-app.post('/tarefas', (req, res) => {
-    const { tarefa } = req.body;
-    db.run("INSERT INTO tarefas (tarefa) VALUES (?)", [tarefa], function(err) {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.status(201).json({ id: this.lastID, tarefa });
-    });
-});
-
-app.get('/tarefas', (req, res) => {
-    db.all("SELECT * FROM tarefas", [], (err, rows) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.status(200).json(rows);
-    });
-});
-
-app.get('/tarefas/:id', (req, res) => {
-    const { id } = req.params;
-    db.get("SELECT * FROM tarefas WHERE id = ?", [id], (err, row) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        if (row) {
-            res.status(200).json(row);
-        } else {
-            res.status(404).json({ error: 'Tarefa não encontrada!' });
-        }
-    });
-});
-
-app.put('/tarefas/:id', (req, res) => {
-    const { id } = req.params;
-    const { tarefa } = req.body;
-    db.run("UPDATE tarefas SET tarefa = ? WHERE id = ?", [tarefa, id], function(err) {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        if (this.changes) {
-            res.status(200).json({ message: 'Tarefa atualizada com sucesso!' });
-        } else {
-            res.status(404).json({ error: 'Tarefa não encontrada!' });
-        }
-    });
-});
-
-app.delete('/tarefas/:id', (req, res) => {
-    const { id } = req.params;
-    db.run("DELETE FROM tarefas WHERE id = ?", [id], function(err) {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        if (this.changes) {
-            res.status(200).json({ message: 'Tarefa removida com sucesso!' });
-        } else {
-            res.status(404).json({ error: 'Tarefa não encontrada!' });
-        }
-    });
-});
-
 
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
 });
 
+module.exports = app;
